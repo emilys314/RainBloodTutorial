e77ff24a6d9b265f5014a27ca70b387c0aa9ff74
emilys314 <emilys314@protonmail.com>
2023-01-29 17:15:02 -0500

    setup compile for cs files

---
+++ Source/RainingBlood.cs
@@ -0,0 +1,8 @@
+namespace RainingBlood {
+    [Verse.StaticConstructorOnStartup]
+    public static class RainingBlood {
+        static RainingBlood() {
+            Verse.Log.Message("Hello, world!");
+        }
+    }
+}
